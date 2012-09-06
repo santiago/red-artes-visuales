798372e8f916e7d22a373096a3be7ec20e3e2e52
Santiago GL <sgaviria@gmail.com>
2012-09-06 20:14:30 +0000

    devel branch borns

--- domain/Admin.js
+++ domain/Admin.js
@@ -1,190 +1,212 @@
 function Service(app) {
-    var Creativo = app.db.model('Creativo');
-    var Usuario = app.db.model('Usuario');
-    var TallerBase = app.db.model('TallerBase');
-    var Taller = app.db.model('Taller');
-    var Evaluacion = app.db.model('Evaluacion');
-    
-    function getCreativos(req, res, next) {
-        var query = (function() {
-            if (req.params.id) {
-                return { _id: req.params.id }
-            }
-            if (req.query) {
-              return req.query;
-            }
-            return {};
-        })();
-        
-        // Find by Id
-        if (query._id) {
-            Creativo.findOne(query, function(err, r) {
-                req.creativo= r;
-                next();
-            });
-        }
-        // Find by All
-        else {
-            Creativo.find(query, function(err, records) {
-                req.creativos = records;
-                next();
-            });
-        }
-    }
-  
-    function getTaller_Bases(req,res,next) {
-      var query = (function() {
-        if (req.params.id) {
-          return {_id: req.params.id}
-        }
-        if (req.query) {
-          return req.query;
-        }
-        return{};
-      })();
+	var Creativo = app.db.model('Creativo');
+	var Usuario = app.db.model('Usuario');
+	var TallerBase = app.db.model('TallerBase');
+	var Taller = app.db.model('Taller');
+	var Evaluacion = app.db.model('Evaluacion');
 
-      if(query._id) {
-        TallerBase.findOne(query, function(err, r) {
-          req.taller_base = record;
-          next();
-        });
-      } else {
-        Taller.find(query, function(err, talleres) {
-          req.talleres = talleres;
-          var id_array = new Array();
-          req.talleres.forEach(function(item,index) {
-            id_array[index] = item.actividad_id;
-          });
-          TallerBase.find({'_id': { $in: id_array}}, function(err, records) {
-            req.taller_bases = records;
-            checkInfoProvided(req.taller_bases, req);
-            next();
-          });
-        });
-      }
-    }
+	function getCreativos(req, res, next) {
+		var query = (function() {
+			if (req.params.id) {
+				return {
+					_id: req.params.id
+				}
+			}
+			if (req.query) {
+				return req.query;
+			}
+			return {};
+		})();
 
-    function checkInfoProvided(taller_bases, req) {
-      var info_provided = new Array();
-      for (var i=0; i<taller_bases.length; i++) {
-        info_provided[i] = i%2;
-      }
-      req.talleres_info_provided = info_provided;
-    }   
+		// Find by Id
+		if (query._id) {
+			Creativo.findOne(query, function(err, r) {
+				req.creativo = r;
+				next();
+			});
+		}
+		// Find by All
+		else {
+			Creativo.find(query, function(err, records) {
+				req.creativos = records;
+				next();
+			});
+		}
+	}
 
-    function postCreativos(req, res, next) {
-        var crypto = require('crypto');
-        var crypt_key = app.crypt_key;
-        var data = req.body;
-        var model = new Creativo(data);
-        var default_passwd = req.body.cedula;
-        var hashed_default_passwd = crypto.createHmac('sha1', crypt_key).update(default_passwd).digest('hex');
-        var user = new Usuario({email: req.body.email, rol:'creativo', cedula: req.body.cedula, contrasena: hashed_default_passwd});
-        user.save(function(err, u) {
-          if (err) {
-            throw err;
-          }
-          model.save(function(err, r) {
-            if (err) {
-              throw err;
-            }
-            req.creativo = r;
-            next();
-          });
-        });
-    }
+	function getTaller_Bases(req, res, next) {
+		var query = (function() {
+			if (req.params.id) {
+				return {
+					_id: req.params.id
+				}
+			}
+			if (req.query) {
+				return req.query;
+			}
+			return {};
+		})();
 
-    function putCreativos(req, res, next) {
-        var data = req.body;
-        var id = req.params.id;
-        Creativo.update({ _id: id }, data, function(err, r) {
-            if (err) {
-                res.error = true;
-            }
-            next();
-        });
-    }
+		if (query._id) {
+			TallerBase.findOne(query, function(err, r) {
+				req.taller_base = record;
+				next();
+			});
+		}
+		else {
+			Taller.find(query, function(err, talleres) {
+				req.talleres = talleres;
+				var id_array = new Array();
+				req.talleres.forEach(function(item, index) {
+					id_array[index] = item.actividad_id;
+				});
+				TallerBase.find({
+					'_id': {
+						$in: id_array
+					}
+				}, function(err, records) {
+					req.taller_bases = records;
+					checkInfoProvided(req.taller_bases, req);
+					next();
+				});
+			});
+		}
+	}
 
-    function delCreativos(req, res, next) {
-        var id = req.params.id;
-        Creativo.findByIdAndRemove(id, function(err, r) {
-            next();
-        });
-    }
+	function checkInfoProvided(taller_bases, req) {
+		var info_provided = new Array();
+		for (var i = 0; i < taller_bases.length; i++) {
+			info_provided[i] = i % 2;
+		}
+		req.talleres_info_provided = info_provided;
+	}
 
-    /*
+	function postCreativos(req, res, next) {
+		var crypto = require('crypto');
+		var crypt_key = app.crypt_key;
+		var data = req.body;
+		var model = new Creativo(data);
+		var default_passwd = req.body.cedula;
+		var hashed_default_passwd = crypto.createHmac('sha1', crypt_key).update(default_passwd).digest('hex');
+		var user = new Usuario({
+			email: req.body.email,
+			rol: 'creativo',
+			cedula: req.body.cedula,
+			contrasena: hashed_default_passwd
+		});
+		user.save(function(err, u) {
+			if (err) {
+				throw err;
+			}
+			model.save(function(err, r) {
+				if (err) {
+					throw err;
+				}
+				req.creativo = r;
+				next();
+			});
+		});
+	}
+
+	function putCreativos(req, res, next) {
+		var data = req.body;
+		var id = req.params.id;
+		Creativo.update({
+			_id: id
+		}, data, function(err, r) {
+			if (err) {
+				res.error = true;
+			}
+			next();
+		});
+	}
+
+	function delCreativos(req, res, next) {
+		var id = req.params.id;
+		Creativo.findByIdAndRemove(id, function(err, r) {
+			next();
+		});
+	}
+
+/*
      * JSON
      */
-    app.get('/admin/creativos.json', getCreativos, function(req, res) {
-        res.send(req.equipamientos);
-    });
-    
-    app.get('/admin/creativo/:id.json', getCreativos, function(req, res) {
-        res.send(req.equipamiento);
-    });
-    
-    app.post('/admin/creativos', postCreativos, function(req, res) {
-        res.send(req.equipamiento, 201);
-    });
+	app.get('/admin/creativos.json', getCreativos, function(req, res) {
+		res.send(req.equipamientos);
+	});
 
-    app.put('/admin/creativos/:id', putCreativos, function(req, res) {
-        if (req.error) res.send({ 'error': true }, 500);
-        else res.send({ 'ok': true });
-    });
+	app.get('/admin/creativo/:id.json', getCreativos, function(req, res) {
+		res.send(req.equipamiento);
+	});
 
-    app.del('/admin/creativo/:id', delCreativos, function(req, res) {
-        res.send({ 'ok': true });
-    });
+	app.post('/admin/creativos', postCreativos, function(req, res) {
+		res.send(req.equipamiento, 201);
+	});
 
-    /*
+	app.put('/admin/creativos/:id', putCreativos, function(req, res) {
+		if (req.error) res.send({
+			'error': true
+		}, 500);
+		else res.send({
+			'ok': true
+		});
+	});
+
+	app.del('/admin/creativo/:id', delCreativos, function(req, res) {
+		res.send({
+			'ok': true
+		});
+	});
+
+	/*
      * HTML
      */
-    app.get('/admin/creativos', getCreativos, function(req, res) {
-        console.log('###########-/admin/creativos-#########');
-        res.render('admin/creativos', {
-            locals: {
-		            articulo: 'Creativos',
-                creativos: req.creativos
-            }
-        });
-    });
+	app.get('/admin/creativos', getCreativos, function(req, res) {
+		console.log('###########-/admin/creativos-#########');
+		res.render('admin/creativos', {
+			locals: {
+				articulo: 'Creativos',
+				creativos: req.creativos
+			}
+		});
+	});
 
-    app.get('/consultas/creativos', getCreativos, function(req, res) {
-        res.render('admin/creativos', {
-            layout: false,
-            locals: {
-            		articulo: 'Creativos',
-                creativos: req.creativos
-            }
-        });
-    });
+	app.get('/consultas/creativos', getCreativos, function(req, res) {
+		res.render('admin/creativos', {
+			layout: false,
+			locals: {
+				articulo: 'Creativos',
+				creativos: req.creativos
+			}
+		});
+	});
 
 
-    app.get('/admin/creativos/new', getCreativos, function(req, res) {
-        res.render('admin/creativo', {
-            locals: {
-          		articulo: 'FormCreativo',
-		          params: app.params
-	          }
-        });
-    });
-    
-    app.get('/admin/creativos/:id', getCreativos, function(req, res) {
-        res.render('admin/creativo', {
-            locals: {
-                creativo: req.creativo
-            }
-        });
-    });
+	app.get('/admin/creativos/new', getCreativos, function(req, res) {
+		res.render('admin/creativo', {
+			locals: {
+				articulo: 'FormCreativo',
+				params: app.params
+			}
+		});
+	});
 
-    app.get('/admin/monitor',getTaller_Bases, function(req,res) {
-      res.render('admin/monitor', {
-        locals: {
-          taller_bases: req.taller_bases,
-          talleres_info_provided: req.talleres_info_provided
-        }
-      });
-    }); 
+	app.get('/admin/creativos/:id', getCreativos, function(req, res) {
+		res.render('admin/creativo', {
+			locals: {
+				creativo: req.creativo
+			}
+		});
+	});
+
+	app.get('/admin/monitor', getTaller_Bases, function(req, res) {
+		res.render('admin/monitor', {
+			locals: {
+				taller_bases: req.taller_bases,
+				talleres_info_provided: req.talleres_info_provided
+			}
+		});
+	});
 }
 
 module.exports = Service;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var env = process.env.NODE_ENV || 'development';
 
 var express = require('express');
+var MongoStore = require('connect-mongo')(express);
 var stylus = require('stylus');
 var everyauth = require('everyauth');
 var app = module.exports = express.createServer();
@@ -16,84 +17,102 @@ everyauth.debug = true;
 //Intialize authentication with username/password
 require('./lib/Auth')(app)
 
-everyauth.everymodule.findUserById(function (id, callback) {
-    app.db.model('Usuario').findById(id, callback);    
+everyauth.everymodule.findUserById(function(id, callback) {
+	app.db.model('Usuario').findById(id, callback);
 });
 
 
 // Configuration
+
+
 function compile(str, path) {
-    return stylus(str)
-	.set('filename', path)
+	return stylus(str).set('filename', path)
 	// .set('compress', true);
 }
 
 
-app.configure(function(){
-    this.set('views', __dirname + '/views');
-    this.set('view engine', 'jade');
-    this.set('view options', { layout: 'layout' })
-    this.use(express.bodyParser());
-    this.use(express.logger());
-    this.use(express.methodOverride());
-    this.use(express.cookieParser());
-    this.use(express.session({secret: 'Eah4tfzGAKhr'}));
-    this.use(stylus.middleware({
-	      src: __dirname + '/views'
-        , dest: __dirname + '/public'
-        , compile: compile
-    }));
-    this.use(express.favicon(__dirname + '/public/favicon.ico', { maxAge: 2592000000}));
-    this.use(express.static(__dirname + '/public'));
+app.configure(function() {
+	this.set('views', __dirname + '/views');
+	this.set('view engine', 'jade');
+	this.set('view options', {
+		layout: 'layout'
+	})
+	this.use(express.bodyParser());
+	this.use(express.logger());
+	this.use(express.methodOverride());
+	this.use(express.cookieParser());
+	this.use(express.session({ store: new MongoStore({ url: 'mongodb://173.230.141.159/red-artes-visuales' }), secret: 'Eah4tfzGAKhr' }));
+    /*this.use(express.session({
+		secret: 'Eah4tfzGAKhr'
+	}));*/
+	this.use(stylus.middleware({
+		src: __dirname + '/views',
+		dest: __dirname + '/public',
+		compile: compile
+	}));
+	this.use(express.favicon(__dirname + '/public/favicon.ico', {
+		maxAge: 2592000000
+	}));
+	this.use(express.static(__dirname + '/public'));
 
-    this.use(everyauth.middleware());
+	this.use(everyauth.middleware());
 
-    // Keep this as last one
-    this.use(this.router);
+	// Keep this as last one
+	this.use(this.router);
 
-    this.use(function(req, res, next){
-      res.render('error', { status: 404, error_text: '404 Pagina no encontrada en el sistema', url: req.url });
-    });
-     this.use(function(err, req, res, next){
-     	console.log(err);
-     	res.render('error', {
-             status: err.status || 500
-     	    , error_text: '500 Error interno en el sistema'
-     	    , error: err
-     	});
-     });
+	this.use(function(req, res, next) {
+		res.render('error', {
+			status: 404,
+			error_text: '404 Pagina no encontrada en el sistema',
+			url: req.url
+		});
+	});
+	this.use(function(err, req, res, next) {
+		console.log(err);
+		res.render('error', {
+			status: err.status || 500,
+			error_text: '500 Error interno en el sistema',
+			error: err
+		});
+	});
 
 
 });
 
-app.configure('development', function(){
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+app.configure('development', function() {
+	app.use(express.errorHandler({
+		dumpExceptions: true,
+		showStack: true
+	}));
 });
 
-app.configure('production', function(){
-    app.use(express.errorHandler());
+app.configure('production', function() {
+	app.use(express.errorHandler());
 });
 
 
-app.get('/login', function(req, res){
-    res.render('login', {
-	badLogin: false,
-	loginError: false
-    });
+app.get('/login', function(req, res) {
+	res.render('login', {
+		badLogin: false,
+		loginError: false
+	});
 });
+
+require('./domain/Admin')(app);
 
 app.get('*', function(req, res, next) {
-    var auth = req.session.auth;
+	var auth = req.session.auth;
 
-    if (!auth || !auth.loggedIn) {
-        res.redirect('/login');
-    } else {
-	next();
-    }
+	if (!auth || !auth.loggedIn) {
+		res.redirect('/login');
+	}
+	else {
+		next();
+	}
 });
 
 app.get('/', function(req, res) {
-    res.redirect('/equipamientos');
+	res.redirect('/equipamientos');
 });
 
 // Initialize Domain
@@ -102,14 +121,15 @@ require('./domain/Participantes')(app)
 require('./domain/Talleres')(app)
 require('./domain/Evaluaciones')(app)
 require('./domain/Consultas')(app)
-require('./domain/Admin')(app)
 
 everyauth.helpExpress(app);
 // App starts here
 // Only listen on $ node app.js
 if (!module.parent) {
-    app.listen(6600);
-    console.log("Express server listening on port %d", 6600)
-} else {
-    if (env == 'test') app.listen(6060);
+	app.listen(6600);
+	console.log("Express server listening on port %d", 6600)
+	console.log(env)
+}
+else {
+	if (env == 'test') app.listen(6060);
 }
