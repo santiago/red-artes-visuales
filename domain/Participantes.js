d3be474e9c0061e83d4f4a2d3005760cdfb70774
Santiago GL <sgaviria@gmail.com>
2012-09-07 00:01:35 +0000

    arreglos

--- domain/Participantes.js
+++ domain/Participantes.js
@@ -1,146 +1,159 @@
 function Service(app) {
-    var Participante = app.db.model('Participante');
+	var Participante = app.db.model('Participante');
 
-    function getParticipantes(req, res, next) {
-        var query = (function() {
-            if (req.params.id) {
-                return { _id: req.params.id }
-            }
-            return {};
-        })();
-        
-        // Find by Id
-        if (query._id) {
-            Participante.findOne(query, function(err, r) {
-                req.participante = r;
-                next();
-            });
-        }
-        // Find by All
-        else {
-            Participante.find(query, { sort: '-nombre' }, function(err, records) {
-                req.participantes = records;
-                next();
-            });
-        }
-    }
+	function getParticipantes(req, res, next) {
+		var query = (function() {
+			if (req.params.id) {
+				return {
+					_id: req.params.id
+				}
+			}
+			return {};
+		})();
 
-    function postParticipantes(req, res, next) {
-        var data = req.body;
-        var model = new Participante(data);
-        model.save(function(err, r) {
-            req.participante = r;
-            next();
-        });
-    }
+		// Find by Id
+		if (query._id) {
+			Participante.findOne(query, function(err, r) {
+				req.participante = r;
+				next();
+			});
+		}
+		// Find by All
+		else {
+			Participante.find(query).sort({ nombre: 'asc' })
+				.exec(function(err, records) {
+					req.participantes = records;
+					next();
+				});
+		}
+	}
 
-    function putParticipantes(req, res, next) {
-        var data = req.body;
-        var id = req.params.id;
-        Participante.update({ _id: id }, data, function(err, r) {
-            if (err) {
-                res.error = true;
-            }
-            next();
-        });
-    }
+	function postParticipantes(req, res, next) {
+		var data = req.body;
+		var model = new Participante(data);
+		model.save(function(err, r) {
+			req.participante = r;
+			next();
+		});
+	}
 
-    function delParticipantes(req, res, next) {
-        var id = req.params.id;
-        Participante.findByIdAndRemove(id, function(err, r) {
-            next();
-        });
-    }
+	function putParticipantes(req, res, next) {
+		var data = req.body;
+		var id = req.params.id;
+		Participante.update({
+			_id: id
+		}, data, function(err, r) {
+			if (err) {
+				res.error = true;
+			}
+			next();
+		});
+	}
 
-    function getEquipamiento(req, res, next) {
-	var Equipamiento = app.db.model('Equipamiento');
-	Equipamiento.findById(req.params.equipamiento_id, function(err, r) {
-	    req.equipamiento = r;
-	    next();
-	});
-    }
+	function delParticipantes(req, res, next) {
+		var id = req.params.id;
+		Participante.findByIdAndRemove(id, function(err, r) {
+			next();
+		});
+	}
 
-    /*
+	function getEquipamiento(req, res, next) {
+		var Equipamiento = app.db.model('Equipamiento');
+		Equipamiento.findById(req.params.equipamiento_id, function(err, r) {
+			req.equipamiento = r;
+			next();
+		});
+	}
+
+/*
      * JSON
      */
-    app.get('/participantes.json', getParticipantes, function(req, res) {
-        res.send(req.participantes);
-    });
-    
-    app.get('/participantes/:id.json', getParticipantes, function(req, res) {
-        res.send(req.participante);
-    });
-    
-    app.post('/participantes', postParticipantes, function(req, res) {
-        res.send(req.participante, 201);
-    });
+	app.get('/participantes.json', getParticipantes, function(req, res) {
+		res.send(req.participantes);
+	});
 
-    app.put('/participantes/:id', putParticipantes, function(req, res) {
-        if (req.error) res.send({ 'error': true }, 500);
-        else res.send({ 'ok': true });
-    });
+	app.get('/participantes/:id.json', getParticipantes, function(req, res) {
+		res.send(req.participante);
+	});
 
-    app.del('/participantes/:id', delParticipantes, function(req, res) {
-        res.send({ 'ok': true });
-    });
+	app.post('/participantes', postParticipantes, function(req, res) {
+		res.send(req.participante, 201);
+	});
+
+	app.put('/participantes/:id', putParticipantes, function(req, res) {
+		if (req.error) res.send({
+			'error': true
+		}, 500);
+		else res.send({
+			'ok': true
+		});
+	});
+
+	app.del('/participantes/:id', delParticipantes, function(req, res) {
+		res.send({
+			'ok': true
+		});
+	});
 
 
-    /*
+/*
      * HTML
      */
-    app.get('/participantes', getParticipantes, function(req, res) {
-        res.render('participantes', {
-	    locals: {
-		articulo: "Participantes",
-		participantes: req.participantes
-	    }
-        });
-    });
-
-
-    app.get('/consultas/participantes/equipamiento/:id', function(req, res) {
-	Participante.find({'equipamiento_id': req.params.id}, function(err, participantes) {
-            if (err) {
-		console.log(err);
-            }
-            res.render('partials/lista_participantes', {
-		layout: false,
-		locals: {
-                    articulo: 'Participantes',
-                    participantes: participantes
-		}
-            });
+	app.get('/participantes', getParticipantes, function(req, res) {
+		res.render('participantes', {
+			locals: {
+				articulo: "Participantes",
+				participantes: req.participantes
+			}
+		});
 	});
-    });
 
-    app.get('/consultas/participantes', getParticipantes, function(req, res) {
-        res.render('partials/lista_participantes', {
-            layout: false,
-            locals: {
-                articulo: 'Participantes',
-                participantes: req.participantes
-            }
-        });
-    });
 
-    // Agregar Participante a Equipamiento
-    app.get('/equipamientos/:equipamiento_id/participantes/new', getEquipamiento, function(req, res) {
-        res.render('forms/participante', {
-            locals: {
-                equipamiento: req.equipamiento,
-                params: app.params,
-                articulo: 'FormParticipante'
-            }
-        });
-    });
-    
-    app.get('/participantes/:id', getParticipantes, function(req, res) {
-        res.render('participante', {
-            locals: {
-                participante: req.participante
-            }
-        });
-    });
+	app.get('/consultas/participantes/equipamiento/:id', function(req, res) {
+		Participante.find({
+			'equipamiento_id': req.params.id
+		}, function(err, participantes) {
+			if (err) {
+				console.log(err);
+			}
+			res.render('partials/lista_participantes', {
+				layout: false,
+				locals: {
+					articulo: 'Participantes',
+					participantes: participantes
+				}
+			});
+		});
+	});
+
+	app.get('/consultas/participantes', getParticipantes, function(req, res) {
+		res.render('partials/lista_participantes', {
+			layout: false,
+			locals: {
+				articulo: 'Participantes',
+				participantes: req.participantes
+			}
+		});
+	});
+
+	// Agregar Participante a Equipamiento
+	app.get('/equipamientos/:equipamiento_id/participantes/new', getEquipamiento, function(req, res) {
+		res.render('forms/participante', {
+			locals: {
+				equipamiento: req.equipamiento,
+				params: app.params,
+				articulo: 'FormParticipante'
+			}
+		});
+	});
+
+	app.get('/participantes/:id', getParticipantes, function(req, res) {
+		res.render('participante', {
+			locals: {
+				participante: req.participante
+			}
+		});
+	});
 }
 
 module.exports = Service;
