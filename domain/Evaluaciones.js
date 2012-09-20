b843f412cb15d783657cf7a33b8774f0ab9d3e6c
Santiago GL <sgaviria@gmail.com>
2012-09-20 17:54:40 +0000

    ui fixes

--- domain/Evaluaciones.js
+++ domain/Evaluaciones.js
@@ -1,9 +1,5 @@
 function Service(app) {
 	var Evaluacion = app.db.model('Evaluacion');
-	var Taller = app.db.model('Taller');
-	var TallerBase = app.db.model('TallerBase');
-	var Participante = app.db.model('Participante');
-	var Creativo = app.db.model('Creativo');
 
 	function getEvaluaciones(req, res, next) {
 		var query = (function() {
@@ -33,7 +29,6 @@ function Service(app) {
 
 	function postEvaluaciones(req, res, next) {
 		var data = req.body;
-		console.log(data);
 		var model = new Evaluacion();
 		model.participante_id = data.p_id;
 		model.creativo_id = data.creativo_id;
@@ -43,23 +38,21 @@ function Service(app) {
 		model.sensibilidad = data.sensibilidad;
 		model.comunicacion = data.comunicacion;
 		model.apreciacion = data.apreciacion;
-		model.habilidades = new Array();
-		var objeto = new Object();
+		model.actualizado = Date.now();
+		model.habilidades = [];
+		var objeto = {};
 		var cnt = 0;
 		for (var key in data) {
-			//console.log("++++++++++++");
-			//console.log(key);
 			var valor = data[key];
 			if (valor == "") valor = 0;
 			if (key.substring(0, 2) == 'i_') {
 				var hab = key.substring(2);
-				//console.log(hab);
-				//console.log("valor:" + valor); 
 				objeto[hab] = valor;
 				cnt++;
 			}
 		}
 		model.habilidades[0] = objeto;
+		console.log(data);
 		model.save(function(err, r) {
 			req.evaluacion = r;
 			next();
@@ -88,11 +81,10 @@ function Service(app) {
 
 	function getSesion(req, res, next) {
 		var Sesion = app.db.model('Taller');
-		console.log("\n\n\n\n\n");
-		console.log(req.params.taller_id);
 		Sesion.findById(req.params.taller_id, function(err, r) {
 			req.sesion = r;
-			console.log(r);
+            req.creativo_id = r.creativo_id;
+            req.params.equipamiento_id = r.equipamiento_id;
 			next();
 		});
 	}
@@ -130,8 +122,16 @@ function Service(app) {
 
 	function getCreativo(req, res, next) {
 		var Creativo = app.db.model('Creativo');
-		Creativo.findOne(req.session.creativo_id, function(err, r) {
+		Creativo.findOne(req.creativo_id, function(err, r) {
 			req.creativo = r;
+			next();
+		});
+	}
+    
+    function getEquipamiento(req, res, next) {
+    	var Equipamiento = app.db.model('Equipamiento');
+		Equipamiento.findOne(req.equipamiento_id, function(err, r) {
+			req.equipamiento = r;
 			next();
 		});
 	}
@@ -169,12 +169,13 @@ function Service(app) {
 	/*
      * HTML
      */
-	app.get('/taller/:taller_id/participante/:participante_id/evaluacion', getEvaluacion, getSesion, getParticipante, getCreativo, getTallerBase, function(req, res) {
+	app.get('/taller/:taller_id/participante/:participante_id/evaluacion', getEvaluacion, getSesion, getEquipamiento, getParticipante, getCreativo, getTallerBase, function(req, res) {
 		res.render('forms/eval_participante', {
 			locals: {
 				evaluacion: req.evaluacion,
 				params: app.params,
 				participante: req.participante,
+                equipamiento: req.equipamiento,
 				creativo: req.creativo,
 				taller: req.sesion,
 				tallerbase: req.tallerbase,
